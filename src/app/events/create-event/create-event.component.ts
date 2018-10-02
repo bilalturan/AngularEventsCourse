7d4e24bad3c4e8a39d212e2beccfd83813d0f2c9
Bilal Turan <bilal_turan@hotmail.com>
2018-10-02 20:38:09 +0200

    .

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
 })
 export class CreateEventComponent implements OnInit {
 
-  isDirt: boolean = true;
+  isDirt = true;
   constructor(private router: Router) { }
 
   ngOnInit() {
