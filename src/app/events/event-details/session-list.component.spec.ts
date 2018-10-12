f50fe8294d0be42befe298c5cc0f95ed476ecacb
Bilal Turan <bilal_turan@hotmail.com>
2018-10-12 12:39:05 +0200

    ...

---
+++ src/app/events/event-details/session-list.component.spec.ts
@@ -0,0 +1,60 @@
+import { SessionListComponent } from './session-list.component';
+import { Session } from '../models/event';
+
+
+describe('SessionListComponent', () => {
+
+  let component: SessionListComponent;
+  let authServiceMock;
+  let voterServiceMock;
+
+  beforeEach(() => {
+    // authServiceMock = jasmine.createSpyObj('httpMock', ['delete', 'post']);
+    // voterServiceMock = jasmine.createSpyObj('httpMock', ['delete', 'post']);
+
+    component = new SessionListComponent(authServiceMock, voterServiceMock);
+  });
+
+  describe('ngOnChanges', () => {
+    it('should filter the sessions correctly', () => {
+
+      // Arrange
+      component.sessions = [
+        <Session>{name: 'session 1', level: 'intermediate'},
+        <Session>{name: 'session 2', level: 'intermediate'},
+        <Session>{name: 'session 3', level: 'beginner'}
+      ];
+
+      component.filterBy = 'intermediate';
+      component.sortBy = 'name';
+      component.eventId = 3; // Not used
+
+      // Act
+      component.ngOnChanges();
+
+      // Assert
+      expect(component.filteredSessions.length).toBe(2);
+    });
+
+    it('should sort the sessions correctly', () => {
+
+      // Arrange
+      component.sessions = [
+        <Session>{name: 'session 3', level: 'beginner'},
+        <Session>{name: 'session 2', level: 'intermediate'},
+        <Session>{name: 'session 1', level: 'intermediate'}
+      ];
+
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 3; // Not used
+
+      // Act
+      component.ngOnChanges();
+
+      // Assert
+      expect(component.filteredSessions[2].name).toBe('session 3');
+    });
+  });
+
+});
