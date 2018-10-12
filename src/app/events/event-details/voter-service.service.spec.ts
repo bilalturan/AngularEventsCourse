feb1797fd93af54905d37a9b233a233bf569dbac
Bilal Turan <bilal_turan@hotmail.com>
2018-10-12 12:09:42 +0200

    ...

---
+++ src/app/events/event-details/voter-service.service.spec.ts
@@ -0,0 +1,48 @@
+import {VoterService} from './voter-service.service';
+import {Session} from '../models/event';
+import { of } from 'rxjs';
+
+
+describe('VoterService', () => {
+
+  let voterService: VoterService;
+  let httpMock;
+
+  beforeEach(() => {
+     // The mock will have a delete and post method
+    httpMock = jasmine.createSpyObj('httpMock', ['delete', 'post']);
+    voterService = new VoterService(httpMock);
+  });
+
+  describe('deleteVoter', () => {
+
+    it('should remove voter from list', () => {
+      // Arrange
+      const session = { id: 6, voters: ['Joe', 'John'] };
+      // Setup mock to return observable of false for delete method
+      httpMock.delete.and.returnValue(of(false));
+
+      // Act
+      voterService.deleteVoter(3, <Session>session, 'Joe');
+
+      // Assert
+      expect(session.voters.length).toBe(1);
+      expect(session.voters[0]).toBe('John');
+    });
+
+    it('should call server with the right URL', () => {
+       // Arrange
+       const session = { id: 6, voters: ['Joe', 'John'] };
+       // Setup mock to return observable of false for delete method
+       httpMock.delete.and.returnValue(of(false));
+
+       // Act
+       voterService.deleteVoter(3, <Session>session, 'Joe');
+
+       // Assert
+       expect(httpMock.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/Joe');
+    });
+
+  });
+
+});
--- src/app/events/event-details/voter-service.service.ts
+++ src/app/events/event-details/voter-service.service.ts
@@ -18,12 +18,8 @@ export class VoterService {
   addVoter(eventId: number, session: Session, voterName: string): void {
     session.voters.push(voterName);
 
-    const options = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    };
-
     const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-    this.http.post(url, {}, options)
+    this.http.post(url, {})
         .pipe(catchError(this.handleError<Session>('addVoter')))
         .subscribe();
   }
@@ -31,12 +27,8 @@ export class VoterService {
   deleteVoter(eventId: number, session: Session, voterName: string): void {
     session.voters = session.voters.filter(voter => voter !== voterName);
 
-    const options = {
-      headers: new HttpHeaders({ 'Content-Type': 'application/json'})
-    };
-
     const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
-    this.http.delete(url, options)
+    this.http.delete(url)
         .pipe(catchError(this.handleError<Session>('addVoter')))
         .subscribe();
   }
