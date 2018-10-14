eb113146197b349590f1efc06b4d38109b376392
Bilal Turan <bilal_turan@hotmail.com>
2018-10-14 07:52:09 +0200

    ...

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -6,6 +6,6 @@ export class AppPage {
   }
 
   getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+    return element(by.css('div.navbar-header a.navbar-brand')).getText();
   }
 }
