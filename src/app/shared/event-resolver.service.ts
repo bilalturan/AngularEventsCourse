de714b89fc38bb2ba9130d412583d7e707433e61
Bilal Turan <bilal_turan@hotmail.com>
2018-10-20 00:15:19 +0200

    ..

--- src/app/shared/event-resolver.service.ts
+++ src/app/shared/event-resolver.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { EventService } from './event.service';
+import { Observable } from 'rxjs';
+
+import {Event} from '../events/models/event';
 
 @Injectable({
   providedIn: 'root'
 })
-export class EventResolverService implements Resolve<any> {
+export class EventResolverService implements Resolve<Event> {
 
   constructor(private eventService: EventService) { }
 
-  resolve(route: ActivatedRouteSnapshot) {
+  resolve(route: ActivatedRouteSnapshot): Observable<Event> {
     // IMPORTANT NOTE: resolver will automatically subsribes to
     // the observable. Other places we would have to call .subscribe().
     return this.eventService.getEvent(route.params['id']);
